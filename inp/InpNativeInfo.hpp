de4638cad775bb45193ae3914bfdbb26886466e0
ToruNiina <niina.toru.68u@gmail.com>
2015-10-24 21:03:34 +0900

    :ok: inputfile nativeinfo block

--- inp/InpNativeInfo.hpp
+++ inp/InpNativeInfo.hpp
@@ -1,6 +1,8 @@
 #ifndef INP_NINFO
 #define INP_NINFO
 #include "InpBase.hpp"
+#include <cstdlib>
+#include <map>
 
 namespace liberica
 {
@@ -15,8 +17,10 @@ namespace liberica
 //  NINFO(M/M)
 //  1 = file1.ninfo
 //  ...
-//  M^2 = fileM^2.ninfo
+//  M^2 = fileM_M.ninfo
 //  >>>> 
+
+    typedef std::pair<int, int> iUnits;
 
     class InpNativeInfo : public InpBase
     {
@@ -26,7 +30,7 @@ namespace liberica
         {
             ALL_IN_ONE,
             ONE_BY_ONE
-        }
+        };
 
     private:
 
@@ -41,22 +45,28 @@ namespace liberica
         ~InpNativeInfo(){}
 
         bool read_file(std::string file_name);
-        std::map<std::pair<int, int>, std::string> const get_files(){return ninfo_files;}
+
+        std::map<std::pair<int, int>, std::string>& get_files()
+        {
+            return ninfo_files;
+        }
 
     private:
 
         void read_block(std::ifstream& ifs);
+
     };
 
     bool InpNativeInfo::read_file(std::string file_name)
     {
         if(already_read)
-            std::cout << "Warning: trying to substitute NativeInfo for already initiated object." << std::endl;
+            std::cout << "Warning: trying to substitute ninfo for already initiated object." 
+                      << std::endl;
         already_read = true;
-        std::ifstream inpfile( file_name.c_str() );
+        std::ifstream inpfile(file_name.c_str());
         bool read(false);
         
-        while(!inpfile.eof() )
+        while(!inpfile.eof())
         {
             std::string line;
             std::getline(inpfile, line);
@@ -64,10 +74,10 @@ namespace liberica
             if(line.empty()) continue;
             if(line[0] == '\x2a') continue;
 
-            if(line.substr(0,20) == "<<<< Native_info_sim")
+            if(eq_ignorecase(line.substr(0,20), "<<<< native_info_sim"))
             {
                 read = true;
-                simN = atoi( line[21] );
+                simN = std::atoi(&line[21]);
                 read_block(inpfile);
             }
         }
@@ -76,7 +86,68 @@ namespace liberica
 
     void InpNativeInfo::read_block(std::ifstream& ifs)
     {
+        bool read(false);
+        while(!ifs.eof())
+        {
+            std::string line;
+            std::getline(ifs, line);
+            if(line.empty()) continue;
+            if(line[0] == '\x2a') continue;
+
+            if(eq_ignorecase(line.substr(0,14), "NINFO(all/all)"))
+            {
+                style = ALL_IN_ONE;
+                iUnits iunits = std::make_pair(0,0);
+                std::vector<std::string> subline = stringsplit(line, ' ');
+                for(auto iter = subline.begin(); iter != subline.end(); ++iter)
+                {
+                    if(iter->substr(iter->size()-6, 6) == "\x2eninfo")
+                    {
+                        read = true;
+                        ninfo_files[iunits] = *iter;
+                        return;
+                    }
+                }
+                throw std::invalid_argument("read_block: cant find ninfo file");
+            }
+            else if(line.substr(0,6) == "NINFO(")
+            {
+                if(style != ONE_BY_ONE)
+                    style = ONE_BY_ONE;
+                std::vector<std::string> iunit1_str
+                    = stringsplit(line, '/', 6);
+                std::vector<std::string> iunit2_str
+                    = stringsplit(iunit1_str.at(1), ')');
+
+                int iunit1(std::stoi(iunit1_str.at(0)));
+                int iunit2(std::stoi(iunit2_str.at(0)));
+
+                std::vector<std::string> filename
+                    = stringsplit(line, ' ');
+                iUnits iunits = std::make_pair(iunit1, iunit2);
+                for(auto itr = filename.begin(); itr != filename.end(); ++itr)
+                {
+                    if(itr->substr(itr->size()-6, 6) == "\x2eninfo")
+                    {
+                        read = true;
+                        ninfo_files[iunits] = *itr;
+                        break;
+                    }
+                }
+            }
+            else if(line.substr(0,4) == ">>>>")
+            {
+                read = true;
+                break;
+            }
+            else
+            {
+                std::cout << "Error: Unknown line" << std::endl;
+                std::cout << line << std::endl;
+            }
+        }
         return;
     }
+
 }
 #endif
